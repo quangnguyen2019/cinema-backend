ffc9faaeeaa3943741d9bb4d43903c9ea6677a19
QuangNguyen <nmquang2012@gmail.com>
2019-06-17 15:09:20 +0700

    update showtime api: getDetailShowtime - restore bookingapi: getDetailBooking

--- api/controllers/booking.controller.js
+++ api/controllers/booking.controller.js
@@ -10,17 +10,11 @@ module.exports.GetBookings = async function (req, res) {
 
 module.exports.GetDetailBooking = async function(req, res) {
     const id = req.params.id;
-    const query = 'select distinct mv.name as movie_name, cgp.name as cinema_group_name, st.start_time ' +
-                  'from "Cinema" as cin join "Cinema_Group" as cgp on (cin.cinema_group_id = cgp.id) ' +
-                        'join "Showtime" as st on (cin.id = st.cinema_id) ' +
-                        'join "Movie" as mv on (st.movie_id = mv.id) ' +
-                  'where st.id = ' + id;
-
-    var booking = null;
     
-    await db.query(query, { type: Sequelize.QueryTypes.SELECT}).then(result => {
-        console.log(result);
-        booking = result;
+    const booking = await Booking.findOne({
+        where: {
+            id: id
+        }
     });
 
     res.json(booking);
--- api/controllers/showtime.controller.js
+++ api/controllers/showtime.controller.js
@@ -1,3 +1,6 @@
+const Sequelize = require('sequelize');
+
+const db = require('../../models/db');
 const Showtime = require('../../models/showtime.model');
 
 module.exports.GetShowtimes = async function (req, res) {
@@ -7,11 +10,16 @@ module.exports.GetShowtimes = async function (req, res) {
 
 module.exports.GetDetailShowtime = async function(req, res) {
     const id = req.params.id;
+    const query = 'select distinct mv.name as movie_name, cgp.name as cinema_group_name, st.start_time ' +
+                  'from "Cinema" as cin join "Cinema_Group" as cgp on (cin.cinema_group_id = cgp.id) ' +
+                        'join "Showtime" as st on (cin.id = st.cinema_id) ' +
+                        'join "Movie" as mv on (st.movie_id = mv.id) ' +
+                  'where st.id = ' + id;
 
-    const showtime = await Showtime.findOne({
-        where: {
-            id: id
-        }
+    var showtime = null;
+    
+    await db.query(query, { type: Sequelize.QueryTypes.SELECT}).then(result => {
+        showtime = result;
     });
 
     res.json(showtime);
